1fc85a2bf87e6050e1a0e67e6cd0610081702374
GMhabib <72284417+GMhabib@users.noreply.github.com>
2020-11-14 15:57:10 +0700

    Create index.bash
    this online script

---
+++ index.bash
@@ -0,0 +1,10 @@
+#!/bin/bash 
+pkg install pv -y 
+sleep 1
+clear 
+echo ""
+echo "welcome in online termux,\ ada yang bisa saya bantu? " | pv -qL 20
+echo ""
+echo "created by habib[VGC] "
+sleep 5
+clear
