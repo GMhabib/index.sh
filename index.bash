c3a00c871df902e99030edb58110aa26e8ec5d24
GMhabib <72284417+GMhabib@users.noreply.github.com>
2020-11-14 18:43:01 +0700

    Update index.bash

--- index.bash
+++ index.bash
@@ -1,10 +1,11 @@
 #!/bin/bash 
-pkg install pv -y 
-sleep 1
+
+
 clear 
 echo ""
-echo "welcome in online termux,\ ada yang bisa saya bantu? " | pv -qL 20
+echo "welcome in online termux, ada yang bisa saya bantu? " | pv -qL 20
 echo ""
-echo "created by habib[VGC] "
+echo "created by habib[VGC] " | pv -qL 20
+
 sleep 5
 clear
